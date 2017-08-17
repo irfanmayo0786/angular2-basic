9a630f11ca56e36e57eb8620f1fdd19351533525
Ankur sharma <restlessankur@gmail.com>
2017-08-17 11:48:34 +0000

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ButtonModule} from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
 
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
