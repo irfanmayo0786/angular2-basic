abc95bfe620bbb98012ce131c8ffb5a60aaddaaf
irfanmayo <Irfan0786@gmail.com>
2017-08-18 15:03:24 +0500

    Bootstrap installed and removed the bootstrap issue tether

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {ButtonModule} from 'primeng/primeng';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 
 import { SidebarComponent } from './sidebar/sidebar.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,SidebarComponent
   ],
   imports: [
-    BrowserModule,ButtonModule
+    BrowserModule,
+    ButtonModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
